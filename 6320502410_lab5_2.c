269e84c7a35c24e440aa68f2bb69a9e915383b80
Nitiphat2 <moozknot@gmail.com>
2021-03-11 14:55:41 +0700

    5

--- 6320502410_lab5_2.c
+++ 6320502410_lab5_2.c
@@ -1,5 +1,74 @@
 #include<stdio.h>
 int main()
 {
+    int n;
+    scanf("%d",&n);
+    int i=0,j,k,a,b;
+    if(n%2==0)
+    {
+        a=n/2;
+        b=i;
+        for(i=0; i<n; i++)
+        {
+            for(j=0; j<n/2; j++)
+            {
+                if(i<a)
+                {
+                    if(j!=a-i-1)
+                    {
+                        printf("_");
+                    }
+                    else
+                    {
+                        printf("*");
+                    }
+                }
+                else
+                {
+                    if(j!=b)
+                    {
+                        printf("_");
+                    }
+                    else
+                    {
+                        printf("*");
+                    }
+                }
+            }
+            for(j=0; j<n/2; j++)
+            {
+                if(j<a)
+                {
+                    if(j!=i)
+                    {
+                        printf("_");
+                    }
+                    else
+                    {
+                        printf("*");
+                    }
+                }
+                else
+                {
+                    if(j!=a-b-1)
+                    {
+                        printf("_");
+                    }
+                    else
+                    {
+                        printf("*");
+                    }
+                }
+            }
+            if(i>=3)
+            {
+                b++;
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
 
+    }
 }
