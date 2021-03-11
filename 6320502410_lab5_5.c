fb425791754060f14b942bb810b04159e5371f0d
Nitiphat2 <moozknot@gmail.com>
2021-03-11 17:34:43 +0700

    dasd

--- 6320502410_lab5_5.c
+++ 6320502410_lab5_5.c
@@ -3,14 +3,29 @@ int main()
 {
     int n,m;
     scanf("%d%d",&n,&m);
-    int num[n][n],sum[(n-1)*(n-1)];
-    int i,j,k;
+    int num[n][n],sum[n-1][n-1];
+    int i,j,k=0,a,l;
     for(i=0; i<n; i++)
     {
         for(j=0; j<n; j++)
         {
-            scanf("%d",num[i][j]);
+            scanf("%d",&num[i][j]);
         }
     }
-
+    for(i=0;i<n-(m-1);i++)
+    {
+        for(j=0; j<n-(m-1); j++)
+        {
+            sum[i][j]=0;
+            for(l=0;l<m;l++)
+            {
+                for(a=0;a<m;a++)
+                {
+                    sum[i][j]=sum[i][j]+num[i+l][j+a];
+                }
+            }
+            printf("%d ",sum[i][j]);
+        }
+        printf("\n");
+    }
 }
