10295b93a554cc1307a2874f0b49fa7d29084b66
Nitiphat2 <moozknot@gmail.com>
2021-03-11 16:36:45 +0700

    52563

--- 6320502410_lab5_1.c
+++ 6320502410_lab5_1.c
@@ -5,9 +5,31 @@ int main()
 {
     int n,i,j,l,a=0,b;
     scanf("%d",&n);
-    char num[n],s[n];
-    int m[n];
+    char num[n],m[n];
+    int f[n];
     for(i=0; i<n; i++)
     {
+        f[a]=0;
+        scanf(" %s",num);
+        if(!(strcmp(num,"+")))
+        {
+
+        }
+        else if(!(strcmp(num,"-")))
+        {
+
+        }
+        else if(!(strcmp(num,"*")))
+        {
+
+        }
+        else
+        {
+            j=strlen(num);
+            for(l=0;l<j;l++)
+            {
+
+            }
+        }
     }
 }
