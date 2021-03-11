1e2a0dcf507aaabd29745072b705a546ce316da7
Nitiphat2 <moozknot@gmail.com>
2021-03-11 15:23:46 +0700

    8

--- 6320502410_lab5_3.c
+++ 6320502410_lab5_3.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a=2,b,i,j=0,k=0;
+    while(k!=n)
+    {
+        j=0;
+        for(i=2;i<a/2;i++)
+        {
+            if(a%i==0)
+            {
+                j=1;
+                break;
+            }
+        }
+        if(j==0)
+        {
+            k++;
+            b=a;
+        }
+        a++;
+    }
 
+}
