f8151cf3e7baabd356e707f2e79e2ffc9e38b10c
Nitiphat2 <moozknot@gmail.com>
2021-03-11 15:43:16 +0700

    s

--- 6320502410_lab5_3.c
+++ 6320502410_lab5_3.c
@@ -3,16 +3,27 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int a=2,b,i,j=0,k=0;
+    int a=1,b=1,i,j=0,k=0,l,z[3]={2,3,5},x;
     while(k!=n)
     {
         j=0;
-        for(i=2;i<a/2;i++)
+        for(i=2;i<a;i++)
         {
             if(a%i==0)
             {
-                j=1;
-                break;
+                x=1;
+                for(l=0;l<3;l++)
+                {
+                    if(i==z[l])
+                    {
+                        x=0;
+                    }
+                }
+                if(x==1)
+                {
+                    j=1;
+                    break;
+                }
             }
         }
         if(j==0)
@@ -20,7 +31,8 @@ int main()
             k++;
             b=a;
         }
+         printf("%d %d\n",a,k);
         a++;
     }
-
+    printf("%d",b);
 }
