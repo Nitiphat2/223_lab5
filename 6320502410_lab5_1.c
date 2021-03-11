c0b93c1e54c9bb9c71aaa2e8dc0546df3dfc4d1c
Nitiphat2 <moozknot@gmail.com>
2021-03-11 16:48:18 +0700

    เเก้

--- 6320502410_lab5_1.c
+++ 6320502410_lab5_1.c
@@ -3,7 +3,7 @@
 #include<math.h>
 int main()
 {
-    int n,i,j,l,a=0,b;
+    int n,i,j,l,a=0,x;
     scanf("%d",&n);
     char num[n],m[n];
     int f[n];
@@ -13,23 +13,40 @@ int main()
         scanf(" %s",num);
         if(!(strcmp(num,"+")))
         {
-
+            f[a-2]=f[a-2]+f[a-1];
+            a--;
         }
         else if(!(strcmp(num,"-")))
         {
-
+            f[a-2]=f[a-2]-f[a-1];
+            a--;
         }
         else if(!(strcmp(num,"*")))
         {
-
+            f[a-2]=f[a-2]*f[a-1];
+            a--;
         }
         else
         {
             j=strlen(num);
+            printf("%d\n",j);
             for(l=0;l<j;l++)
             {
-
+                switch(x)
+                {
+                    case 0 : f[a] = f[a]+((int)num[l] - 48); break;
+                    case 1 : f[a] = f[a]+((int)num[l] - 48) * 10; break;
+                    case 2 : f[a] = f[a]+((int)num[l] - 48) * 100; break;
+                    case 3 : f[a] = f[a]+((int)num[l] - 48) * 1000; break;
+                    case 4 : f[a] = f[a]+((int)num[l] - 48) * 10000; break;
+                    case 5 : f[a] = f[a]+((int)num[l] - 48) * 100000; break;
+                    case 6 : f[a] = f[a]+((int)num[l] - 48) * 1000000; break;
+                    case 7 : f[a] = f[a]+((int)num[l] - 48) * 10000000; break;
+                    case 8 : f[a] = f[a]+((int)num[l] - 48) * 100000000; break;
+                    case 9 : f[a] = f[a]+((int)num[l] - 48) * 1000000000; break;
+                }
             }
+            a++;
         }
     }
 }
